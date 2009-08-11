585a28316e9347d3f2e9d7f2cae1c963b28a42ea
Nic Williams and Durran Jordan <dev+drnic+durran@hashrocket.com>
2009-08-11 17:22:01 -0400

    some comments about what to do + some sample forking code

--- lib/culerity.rb
+++ lib/culerity.rb
@@ -5,6 +5,13 @@ module Culerity
 
   def self.run_server
     IO.popen("jruby #{__FILE__}", 'r+')
+    # pid = fork { exec "jruby #{__FILE__}") }
+    # Process.detach pid
+    
+    # store celerity pid in tmp/culerity_celerity.pid
+    # store server pid in tmp/culerity_rails_server.pid
+    
+    # open named pipes to communicate with celerity_server + return them
   end
   
 end
