64d41edb02731b5787c49f516ce33e1dc459b8f6
Preston Marshall <preston@synergyeoc.com>
2010-09-01 17:53:43 -0500

    Replace Rails 2 Generators with Rails 3 Thor-based generators

---
+++ lib/rails/generators/culerity_generator.rb
@@ -0,0 +1,17 @@
+class CulerityGenerator < ::Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+  desc "Install Culerity into your application"
+  def generate_culerity
+    copy_file 'features/step_definitions/culerity_steps.rb', 'features/step_definitions/culerity_steps.rb'
+    copy_file 'features/support/env.rb', 'features/support/culerity_env.rb'
+    copy_file 'config/environments/culerity.rb', 'config/environments/culerity.rb'
+
+    gsub_file 'config/database.yml', /cucumber:.*\n/, "cucumber: &CUCUMBER\n"
+
+    gsub_file 'config/database.yml', /\z/, "\nculerity:\n  <<: *CUCUMBER"
+    
+    copy_file "lib/tasks/culerity.rake", "lib/tasks/culerity.rake"
+    
+    copy_file 'public/javascripts/culerity.js', 'public/javascripts/culerity.js'
+  end
+end
